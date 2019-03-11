2f981c599155c2fbf9ef5d452659c89af5b1e470
yuki <47917831+loooovingyuki@users.noreply.github.com>
2019-03-12 07:09:49 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization');
+header('Content-type: application/xml');
+header('Location: /index.html');
+?>
